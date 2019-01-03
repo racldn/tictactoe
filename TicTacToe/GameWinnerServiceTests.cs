89114858dfe5605b2f1b70eecb7da8cf5df4a4fe
CAMPBER1 <CAMPBER1@LNGLONL7034881>
2019-01-03 16:39:15 +0000

    first commit

---
+++ TicTacToe/GameWinnerServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TicTacToeServices;
+
+namespace TicTacToe
+
+{
+    [TestFixture]
+    public class GameWinnerServiceTests
+    {
+        [Test]
+        public void NeitherPlayerHasthreeInARow()
+        {
+            IGameWinnerService gameWinnerService;
+            gameWinnerService = new GameWinnerService();
+            const char expected = ' ';
+            var gameboard = new char[3, 3]
+            {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+            };
+
+            var actual = gameWinnerService.Validate(gameboard);
+            Assert.AreEqual(expected, actual);
+        } 
+    }
+}
---
+++ TicTacToeServices/GameWinnerService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeServices
+{
+    public interface IGameWinnerService
+    {
+        char Validate(char[,] gameboard);
+    }
+
+    public class GameWinnerService : IGameWinnerService
+    {
+        public char Validate(char[,] gameboard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+   
+}
